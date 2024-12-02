a3bcb855f73c6852b2221833cbbfd67bb92e5d49
peiqingyang <peiqingyang0530@163.com>
2024-12-02 20:37:11 +0800

    zhuan

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class test1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int s = Integer.parseInt(scan.nextLine());
 
+        scan.close();
     }
 }
 
